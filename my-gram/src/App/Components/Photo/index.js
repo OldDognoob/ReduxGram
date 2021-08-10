d0323d57ae00aa28809b961b3e8703bfd5a99bec
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-08-10 15:02:05 +0300

    changes

---
+++ my-gram/src/App/Components/Photo/index.js
@@ -0,0 +1,8 @@
+import Photo from "./Photo/Photo";
+import Comments from "./Comments/Comments";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  Photo,
+  Comments
+};
