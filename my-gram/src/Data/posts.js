2f014670052dc113f9b9a18db7644927813bfb6f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-08-10 19:18:41 +0300

    wip-posts-file

---
+++ my-gram/src/Data/posts.js
@@ -0,0 +1,100 @@
+const posts = [
+    {
+      code: "cssvEZacHvQ",
+      caption: "Lovely forest",
+      likes: 1056,
+      id: "1161022966406956503",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "j-MPRQOJfVU",
+      caption: "Heroes",
+      likes: 1000000,
+      id: "1160844458347054781",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "CSs8aiN_LkI",
+      caption:
+        "Summer Holidays",
+      likes: 279,
+      id: "1154606670337393752",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "C9XgrB8hqBI",
+      caption: "Moments",
+      likes: 182,
+      id: "1157179863266871229",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "GaBDdA63GcQ",
+      caption:
+        "Work home",
+      likes: 66,
+      id: "1126293663140399053",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "/iANAdaNu7mg",
+      caption: "Good Vibes only",
+      likes: 33,
+      id: "1117418173361145365",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "uX7UOpU-884",
+      caption:
+        "Computer set up",
+      likes: 30,
+      id: "1162418651480049646",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "8Lymqsw5YV4",
+      caption: "The Legend",
+      likes: 40,
+      id: "1152964002473690553",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+    {
+      code: "-pSOAtdMVlk",
+      caption: "Viber",
+      likes: 74,
+      id: "1141561999742846119",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+   
+    {
+      code: "wdc9ZAiwBB4",
+      caption:
+        "",
+      likes: 57,
+      id: "1135147611821557699",
+      display_src: `https://unsplash.com/photos/400/400/?image=${Math.floor(
+        Math.random() * 85
+      )}`
+    },
+  ];
+  
+  export default posts;
+  
